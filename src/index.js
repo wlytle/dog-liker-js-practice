518ee4317f2b3a76dc6374cd981ea18e54cb4903
Hallie Dunn <hallie.dunn1@gmail.com>
2020-03-13 11:42:23 -0700

    new dog form

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const DOGSURL = "http://localhost:3000/dogs"
     const main = document.getElementsByTagName("main")[0]
-    const form = document.getElementsByTagName("form")[0]
+    const newDogForm = document.getElementsByTagName("form")[0]
 
     fetch(DOGSURL)
     .then(res=>res.json())
@@ -11,62 +11,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const populateDogs = (dogs) => {
         dogs.forEach(dog=> {
-            let div = document.createElement('div')
-            div.id = dog.id
-            let h2 = document.createElement('h2')
-            h2.innerText = dog.name
-            let p = document.createElement('p')
-            p.innerText = dog.breed
-            let img = document.createElement('img')
-            img.src = dog.image
-            let p2 = document.createElement('p')
-            p2.id = `${dog.name}-likes`
-            p2.innerText = dog.likes
-            let likesLabel = document.createElement('p')
-            likesLabel.innerText = "Likes:"
-            let btn = document.createElement('button')
-            btn.innerText = "like this doggo"
-            btn.addEventListener('click', ()=>upvoteDoggo(dog))
-            let superBtn = document.createElement('button')
-            superBtn.innerText = "Super Like"
-            superBtn.addEventListener('click', ()=>upvoteDoggo(dog, true))
-            let p3 = document.createElement('p')
-            p3.innerText = "Comments:"
-            let ul = document.createElement('ul')
-            ul.id = `${dog.name}-comments`
-            dog.comments.forEach(comment=> {
-                let li = document.createElement('li')
-                li.innerText = comment
-                ul.appendChild(li)
-            })
-            let commentForm = document.createElement('form')
-            let label = document.createElement('label')
-            label.innerText = "Add Comment:"
-            let input1 = document.createElement('input')
-            input1.placeholder = "text here"
-            input1.type = "text"
-            input1.name = "comment"
-            let input2 = document.createElement('input')
-            input2.type = 'submit'
-            
-            commentForm.addEventListener('submit', (ev)=> submitComment(ev, dog))
-            commentForm.appendChild(label)
-            commentForm.appendChild(input1)
-            commentForm.appendChild(input2)
-
-
-            div.appendChild(h2)
-            div.appendChild(p)
-            div.appendChild(img)
-            div.appendChild(likesLabel)
-            div.appendChild(p2)
-            div.appendChild(btn)
-            div.appendChild(superBtn)
-            div.appendChild(p3)
-            div.appendChild(ul)
-            div.appendChild(commentForm)
-            main.appendChild(div)
+            addDogToPage(dog)
         })
+        let formHeader = document.createElement('h2')
+        formHeader.innerText = "Create a new dog here!"
+        newDogForm.appendChild(formHeader)
+        let labelN = document.createElement('label')
+        labelN.innerText = "Name: "
+        let nameField = document.createElement('input')
+        nameField.name = "dogName"
+        newDogForm.appendChild(labelN)
+        newDogForm.appendChild(nameField)
+        let labelB = document.createElement('label')
+        labelB.innerText = "Breed: "
+        let breedField = document.createElement('input')
+        breedField.name = "breed"
+        newDogForm.appendChild(labelB)
+        newDogForm.appendChild(breedField)
+        let labelI = document.createElement('label')
+        labelI.innerText = "Image URL: "
+        let urlField = document.createElement('input')
+        urlField.name = "image"
+        newDogForm.appendChild(labelI)
+        newDogForm.appendChild(urlField)
+        let newDogSubmit = document.createElement('button')
+        newDogSubmit.type = 'submit'
+        newDogSubmit.innerText = 'Save Doggo'
+        newDogForm.appendChild(newDogSubmit)
+        newDogForm.addEventListener('submit', (event)=> {addDoggo(event)})
     }
     
     const submitComment = (event, dog) => {
@@ -111,6 +83,80 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(json=> {
             p.innerText = json.likes
         })  
+    }
+
+    const addDoggo = (event) => {
+        event.preventDefault()
+        let postData = {likes: 0, name: event.target.children.dogName.value, breed: event.target.children.breed.value, image: event.target.children.image.value, comments: []}
+        fetch(DOGSURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        })
+        .then(res=>res.json())
+        .then(json=> {
+            addDogToPage(json)
+        })  
+    }
+
+    const addDogToPage = (dog) => {
+        let div = document.createElement('div')
+        div.id = dog.id
+        let h2 = document.createElement('h2')
+        h2.innerText = dog.name
+        let p = document.createElement('p')
+        p.innerText = dog.breed
+        let img = document.createElement('img')
+        img.src = dog.image
+        let p2 = document.createElement('p')
+        p2.id = `${dog.name}-likes`
+        p2.innerText = dog.likes
+        let likesLabel = document.createElement('p')
+        likesLabel.innerText = "Likes:"
+        let btn = document.createElement('button')
+        btn.innerText = "like this doggo"
+        btn.addEventListener('click', ()=>upvoteDoggo(dog))
+        let superBtn = document.createElement('button')
+        superBtn.innerText = "Super Like"
+        superBtn.addEventListener('click', ()=>upvoteDoggo(dog, true))
+        let p3 = document.createElement('p')
+        p3.innerText = "Comments:"
+        let ul = document.createElement('ul')
+        ul.id = `${dog.name}-comments`
+        dog.comments.forEach(comment=> {
+            let li = document.createElement('li')
+            li.innerText = comment
+            ul.appendChild(li)
+        })
+        let commentForm = document.createElement('form')
+        let label = document.createElement('label')
+        label.innerText = "Add Comment:"
+        let input1 = document.createElement('input')
+        input1.placeholder = "text here"
+        input1.type = "text"
+        input1.name = "comment"
+        let input2 = document.createElement('input')
+        input2.type = 'submit'
+        
+        commentForm.addEventListener('submit', (ev)=> submitComment(ev, dog))
+        commentForm.appendChild(label)
+        commentForm.appendChild(input1)
+        commentForm.appendChild(input2)
+
+        div.appendChild(h2)
+        div.appendChild(p)
+        div.appendChild(img)
+        div.appendChild(likesLabel)
+        div.appendChild(p2)
+        div.appendChild(btn)
+        div.appendChild(superBtn)
+        div.appendChild(p3)
+        div.appendChild(ul)
+        div.appendChild(commentForm)
+        main.appendChild(div)
     }
     
 })
